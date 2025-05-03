46c5356e365347c6a7951c870ea6f9082a82d019
d3n15tttt <106825618+d3n15tttt@users.noreply.github.com>
2025-05-03 12:23:16 +0300

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) return `ERROR`;
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return `ERROR`;
+    let finalNumber = 0 ;
+    if (num1 < num2) {
+        for( num1; num1 <= num2; num1++) {
+            finalNumber += num1;
+        }
+    } else if (num1 > num2) {
+        for( num2; num2 <= num1; num2++) {
+            finalNumber += num2;
+        }
+    }
+    return finalNumber;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let test;
+  test = Math.round(((num - 32)* 5/9) * 10) / 10;
+  return test;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let test;
+  test = Math.round(((num * 9/5) + 32) * 10) / 10;
+  return test;
 };
 
 // Do not edit below this line
@@ -9,3 +15,8 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+// (°F − 32) × 5/9 = °C
+
+// (°C × 9/5) + 32 = °F
+
+//Math.round(number * 10) / 10
